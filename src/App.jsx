ef05248b6af049131f232a606cf8dd094267d941
jjorgeadrian007 <jjorgeadrian007@gmail.com>
2026-08-06 15:23:22 -0500

    ingresos-cafeteria

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ export default function DashboardAsociacion() {
     { id: 9, fecha: "22/07/2026", nombre: "Aportes Carmen Romero (Mayo, Junio 2026)", valor: 20.00 },
     { id: 10, fecha: "27/07/2026", nombre: "Aportes PAGUAY LOPEZ KLEBER RENE (enero a septiembre 2026)", valor: 65.00 },
     { id: 11, fecha: "31/07/2026", nombre: "Aportes socios Mayo", valor: 472.35 },
+    { id: 12, fecha: "06/08/2026", nombre: "Aportes socios Mayo", valor: 52.50 },
   ]);
 
   const [egresos] = useState([
