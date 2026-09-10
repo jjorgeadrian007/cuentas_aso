a001b7bada6ece93a407b86d09bda6cad8151586
jjorgeadrian007 <jjorgeadrian007@gmail.com>
2026-09-10 07:51:51 -0500

    cuentas-aportes-carmen romero

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ export default function DashboardAsociacion() {
     { id: 13, fecha: "11/08/2026", nombre: "Aportes Priscila Carrera", valor: 70.00 },
     { id: 14, fecha: "18/08/2026", nombre: "Aportes socios Julio", valor: 630.94 },
     { id: 15, fecha: "20/08/2026", nombre: "Aportes Carmen Romero(Julio 2026)", valor: 10.00 },
+    { id: 16, fecha: "10/09/2026", nombre: "Aportes Carmen Romero(Agosto, Septiembre, Octubre 2026)", valor: 30.00 },
     
   ]);
 
