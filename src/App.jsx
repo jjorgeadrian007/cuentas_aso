98a964f6d9d7873eaa45d6cb2066bcacfd435a32
jjorgeadrian007 <jjorgeadrian007@gmail.com>
2026-09-16 11:10:21 -0500

    aportes Agosto

--- src/App.jsx
+++ src/App.jsx
@@ -22,8 +22,9 @@ export default function DashboardAsociacion() {
     { id: 14, fecha: "18/08/2026", nombre: "Aportes socios Julio", valor: 630.94 },
     { id: 15, fecha: "20/08/2026", nombre: "Aportes Carmen Romero(Julio 2026)", valor: 10.00 },
     { id: 16, fecha: "10/09/2026", nombre: "Aportes Carmen Romero(Agosto, Septiembre, Octubre 2026)", valor: 30.00 },
-     { id: 17, fecha: "11/09/2026", nombre: "Aportes Carmen Romero(Noviembre, Diciembre 2026)", valor: 20.00 },
-     { id: 18, fecha: "15/09/2026", nombre: "Aportes Isabel Dávila (Septiembre, Octubre 2026)", valor: 20.00 },
+    { id: 17, fecha: "11/09/2026", nombre: "Aportes Carmen Romero(Noviembre, Diciembre 2026)", valor: 20.00 },
+    { id: 18, fecha: "15/09/2026", nombre: "Aportes Isabel Dávila (Septiembre, Octubre 2026)", valor: 20.00 },
+    { id: 19, fecha: "15/09/2026", nombre: "Aportes socios Agosto", valor: 638.15 },
     
   ]);
 
